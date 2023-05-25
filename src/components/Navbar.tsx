0dc11db7a8c2389957f84d4e31e91c9241b7f96d
MattGomb <gombosonline@gmail.com>
2023-05-25 15:24:48 -0500

    Revert navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,31 @@
+"use client";
+import { useRef } from "react";
+
 const Navbar = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const toggleFunction = () => {
+    const x: HTMLInputElement = ref.current!;
+      x.style.display = "flex";
+  };
   return (
     <div className="flex justify-between px-4 py-2">
-    <p>hamburger placeholder</p>
-    <div className="flex">
-      <p>torre</p>
-      <p>.co</p>
+      <div className="flex gap-4">
+        <p>hamburger placeholder</p>
+        <div className="flex">
+          <p>torre</p>
+          <p>.co</p>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="flex flex-col items-end gap-1">
+          <input id="searchInput" ref={ref} type="text" placeholder="username" className="text-black" />
+          <button onClick={toggleFunction}>search</button>
+        </div>
+        <p>SIGN IN</p>
+      </div>
     </div>
-    <p>search function</p>
-    <p>SIGN IN</p>
-  </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
