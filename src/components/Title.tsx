9c9b5bbc7f4192dfcf4f3d58deeb70a558883fcd
MattGomb <gombosonline@gmail.com>
2023-05-25 15:25:49 -0500

    Update title component to call picture

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,35 @@
-const Title = () => {
-  return (
-    <div className="py-10">Title</div>
-  )
+type propsType = {
+  username: string;
+};
+
+type bioData = {
+  "person": 
+  {
+    'name': string,
+    "pictureThumbnail": string,
+  }
 }
 
-export default Title
+const fetchSkills = async (username: string = "renanpeixotox") => {
+  const res = await fetch(`https://torre.bio/api/bios/${username}`);
+  const data = await res.json();
+  console.log(data.person.name);
+  return data;
+};
+
+const Title = async ({ username }: propsType) => {
+  const data = (await fetchSkills(username)) as unknown as bioData;
+
+  return (
+    <div className="flex flex-col py-10 items-center gap-2">
+      <div className="hex">
+      <div className="flex hex-background items-center align-center justify-center">
+        <img src={data.person.pictureThumbnail} alt="pic thumbnail" />
+      </div>
+      </div>
+      <h3>{data.person.name}</h3>
+    </div>
+  );
+};
+
+export default Title;
