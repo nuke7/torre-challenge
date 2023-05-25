4b92a46cb4c4e2a6469faebd57326a3bb1dfade1
MattGomb <gombosonline@gmail.com>
2023-05-25 15:26:18 -0500

    Update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Navbar from "@/components/Navbar";
 import Skills from "@/components/Skills";
 import Title from "@/components/Title";
 
-export default async function Home() {  
+export default async function Home() {
   return (
-    <>
-      <Title />
+    <main className="flex flex-col text-white w-full sm:w-5/6 md:w-4/6">
+      <Navbar />
       {/* @ts-expect-error Async Server Component */}
-      <Skills />
-    </>
-  )
+      <Title username={"gombosonline"} />
+      {/* @ts-expect-error Async Server Component */}
+      <Skills username={"gombosonline"} />
+    </main>
+
+  );
 }
