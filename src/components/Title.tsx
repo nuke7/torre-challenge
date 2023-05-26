8415102cc85454be5edc7500440887422c8b5edd
MattGomb <gombosonline@gmail.com>
2023-05-26 14:47:09 -0500

    Refactor title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,33 +1,18 @@
-type propsType = {
-  username: string;
-};
+"use client";
 
-type bioData = {
-  "person": 
-  {
-    'name': string,
-    "pictureThumbnail": string,
-  }
-}
+import { useSearchContext } from "@/context/SearchContext";
 
-const fetchSkills = async (username: string = "renanpeixotox") => {
-  const res = await fetch(`https://torre.bio/api/bios/${username}`);
-  const data = await res.json();
-  console.log(data.person.name);
-  return data;
-};
-
-const Title = async ({ username }: propsType) => {
-  const data = (await fetchSkills(username)) as unknown as bioData;
-
+const Title = () => {
+  const data = useSearchContext();
+  
   return (
     <div className="flex flex-col py-10 items-center gap-2">
       <div className="hex">
-      <div className="flex hex-background items-center align-center justify-center">
-        <img src={data.person.pictureThumbnail} alt="pic thumbnail" />
+        <div className="flex hex-background items-center align-center justify-center">
+          <img src="" alt="picture thumbnail" />
+        </div>
       </div>
-      </div>
-      <h3>{data.person.name}</h3>
+      <h3 className="text-xl">Title placeholder</h3>
     </div>
   );
 };
